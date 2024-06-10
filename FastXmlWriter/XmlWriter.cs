ddc0fd54a030c10939d8fe80386dce83deb804a0
Michael Fieldness <mfieldness@gmail.com>
2024-06-10 13:37:59 -0700

    Add project files.

---
+++ FastXmlWriter/XmlWriter.cs
@@ -0,0 +1,125 @@
+﻿using System.Buffers;
+using System.Text;
+
+namespace FastXmlWriter;
+
+public sealed class XmlWriter
+{
+    private readonly IBufferWriter<byte> buffer;
+    private OpenTag openTag = OpenTag.None;
+
+    public XmlWriter(IBufferWriter<byte> buffer)
+    {
+        this.buffer = buffer;
+    }
+
+    public void WriteStartElement(ReadOnlySpan<char> name)
+    {
+        WriteEndOfStartElement();
+        buffer.Write("<"u8);
+        Encoding.UTF8.GetBytes(name, buffer);
+        openTag = OpenTag.Normal;
+    }
+
+    public void WriteStartElement(ReadOnlySpan<byte> name)
+    {
+        WriteEndOfStartElement();
+        buffer.Write("<"u8);
+        buffer.Write(name);
+        openTag = OpenTag.Normal;
+    }
+
+    public void WriteEndElement(ReadOnlySpan<char> name)
+    {
+        WriteEndOfStartElement();
+        openTag = OpenTag.None;
+        buffer.Write("</"u8);
+        Encoding.UTF8.GetBytes(name, buffer);
+        buffer.Write(">"u8);
+    }
+
+    public void WriteEndElement(ReadOnlySpan<byte> name)
+    {
+        WriteEndOfStartElement();
+        openTag = OpenTag.None;
+        buffer.Write("</"u8);
+        buffer.Write(name);
+        buffer.Write(">"u8);
+    }
+
+    public void WriteEmptyElement(ReadOnlySpan<char> name)
+    {
+        WriteEndOfStartElement();
+        buffer.Write("<"u8);
+        Encoding.UTF8.GetBytes(name, buffer);
+        openTag = OpenTag.Empty;
+    }
+
+    public void WriteEmptyElement(ReadOnlySpan<byte> name)
+    {
+        WriteEndOfStartElement();
+        buffer.Write("<"u8);
+        buffer.Write(name);
+        openTag = OpenTag.Empty;
+    }
+
+    public void WriteAttributeName(ReadOnlySpan<char> name)
+    {
+        buffer.Write(" "u8);
+        Encoding.UTF8.GetBytes(name, buffer);
+    }
+
+    public void WriteAttributeName(ReadOnlySpan<byte> name)
+    {
+        buffer.Write(" "u8);
+        buffer.Write(name);
+    }
+
+    public void WriteAttributeValue(ReadOnlySpan<char> value)
+    {
+        buffer.Write("=\""u8);
+        Encoding.UTF8.GetBytes(value, buffer);
+        buffer.Write("\""u8);
+    }
+
+    public void WriteAttributeValue(ReadOnlySpan<byte> value)
+    {
+        buffer.Write("=\""u8);
+        buffer.Write(value);
+        buffer.Write("\""u8);
+    }
+
+    public void WriteText(ReadOnlySpan<char> text)
+    {
+        WriteEndOfStartElement();
+        openTag = OpenTag.None;
+        Encoding.UTF8.GetBytes(text, buffer);
+    }
+
+    public void WriteText(ReadOnlySpan<byte> text)
+    {
+        WriteEndOfStartElement();
+        openTag = OpenTag.None;
+        buffer.Write(text);
+    }
+
+    private void WriteEndOfStartElement()
+    {
+        switch (openTag)
+        {
+            case OpenTag.Normal:
+                buffer.Write(">"u8);
+                break;
+            case OpenTag.Empty:
+                buffer.Write("/>"u8);
+                break;
+        }
+    }
+
+    private enum OpenTag
+    {
+        None,
+        Normal,
+        Empty
+    }
+}
---
+++ Samples/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.IO.Pipelines;
+using FastXmlWriter;
+
+namespace Samples;
+
+internal class Program
+{
+    async static Task Main(string[] _)
+    {
+        using var stream = File.Create("sample.svg", 0);
+        var writer = PipeWriter.Create(stream);
+
+        var xmlWriter = new XmlWriter(writer);
+        xmlWriter.WriteStartElement("svg"u8);
+        xmlWriter.WriteAttributeName("xmlns"u8);
+        xmlWriter.WriteAttributeValue("http://www.w3.org/2000/svg"u8);
+        xmlWriter.WriteAttributeName("width"u8);
+        xmlWriter.WriteAttributeValue("600"u8);
+        xmlWriter.WriteAttributeName("height"u8);
+        xmlWriter.WriteAttributeValue("400"u8);
+        xmlWriter.WriteAttributeName("viewBox"u8);
+        xmlWriter.WriteAttributeValue("-60 -340 600 400"u8);
+
+        xmlWriter.WriteStartElement("g"u8);
+        xmlWriter.WriteAttributeName("transform"u8);
+        xmlWriter.WriteAttributeValue("translate(0.5 -0.5) scale(17.3333 -2)"u8);
+
+        xmlWriter.WriteEmptyElement("path"u8);
+        xmlWriter.WriteAttributeName("d"u8);
+        xmlWriter.WriteAttributeValue("M0 0L0 86L1 88L2 97L5 97L6 99L7 99L8 101L9 102L12 102L13 103L14 106L16 118L19 118L20 118L21 128L22 129L23 129L27 132L28 132L29 133L30 151L30 0"u8);
+        xmlWriter.WriteAttributeName("fill"u8);
+        xmlWriter.WriteAttributeValue("#CC0000"u8);
+
+        xmlWriter.WriteEmptyElement("path"u8);
+        xmlWriter.WriteAttributeName("d"u8);
+        xmlWriter.WriteAttributeValue("M0 0L0 28L1 30L2 34L5 41L6 43L7 48L8 53L9 54L12 63L13 67L14 68L16 73L19 77L20 77L21 77L22 80L23 82L27 83L28 85L29 89L30 93L30 0"u8);
+        xmlWriter.WriteAttributeName("fill"u8);
+        xmlWriter.WriteAttributeValue("#0000DD"u8);
+
+        xmlWriter.WriteEmptyElement("path"u8);
+        xmlWriter.WriteAttributeName("d"u8);
+        xmlWriter.WriteAttributeValue("M0 0L1 5L2 5L5 14L6 14L7 24L8 26L9 28L12 36L13 45L14 45L16 45L19 48L20 50L21 50L22 51L23 54L27 56L28 56L29 63L30 74L30 0"u8);
+        xmlWriter.WriteAttributeName("fill"u8);
+        xmlWriter.WriteAttributeValue("green"u8);
+
+        xmlWriter.WriteEndElement("g"u8);
+
+        xmlWriter.WriteStartElement("g"u8);
+        xmlWriter.WriteAttributeName("stroke"u8);
+        xmlWriter.WriteAttributeValue("black"u8);
+
+        xmlWriter.WriteEmptyElement("path"u8);
+        xmlWriter.WriteAttributeName("d"u8);
+        xmlWriter.WriteAttributeValue("M0 0h520.5M0 0v-320.5"u8);
+
+        xmlWriter.WriteEndElement("g"u8);
+
+        xmlWriter.WriteEndElement("svg"u8);
+
+        await writer.FlushAsync();
+        await writer.CompleteAsync();
+    }
+}
